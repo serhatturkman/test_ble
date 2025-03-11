e6003387be1fb1700f41e7895bac97050fe4c029
serhatturkman <serhat.turkman@bites.com.tr>
2025-03-11 18:10:30 +0300

    third commit

--- gateway/src/main.c
+++ gateway/src/main.c
@@ -10,13 +10,7 @@
 
 /* Gateway Application - BLE Central */
 
-#include <zephyr.h>
-#include <sys/printk.h>
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/gatt.h>
-#include <bluetooth/conn.h>
-#include <device.h>
-#include <drivers/uart.h>
+#include "zephyr.h"
 
 #define CONFIG_UPDATE_INTERVAL K_MINUTES(5)
 #define MAX_SENSOR_DATA_SIZE 2048
@@ -31,18 +25,19 @@ static void bt_ready(int err) {
     printk("Bluetooth initialized\n");
 }
 
-void main(void) {
+int main(void) {
     int err;
     printk("[GATEWAY] Starting BLE Central application\n");
 
     err = bt_enable(bt_ready);
     if (err) {
         printk("Bluetooth initialization failed (err %d)\n", err);
-        return;
+        return 0;
     }
     
     while (1) {
         printk("[GATEWAY] Waiting for Node data...\n");
         k_sleep(K_SECONDS(30));
     }
+    return 0;
 }
