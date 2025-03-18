997a57c3cd225761b4ddc83c9d40d2488f2a0fcc
serhatturkman <serhat.turkman@bites.com.tr>
2025-03-18 14:39:53 +0300

    Twelveth Commit

--- node/src/main.c
+++ node/src/main.c
@@ -1,64 +1,66 @@
-/* main.c - Application main entry point */
-
 /*
- * Copyright (c) 2024 Nordic Semiconductor ASA
- * Copyright (c) 2015-2016 Intel Corporation
+ * Copyright (c) 2020 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
+#include <zephyr/types.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/kernel.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/hci.h>
 #include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
-#include <zephyr/bluetooth/services/bas.h>
-#include <zephyr/bluetooth/services/hrs.h>
+#include <zephyr/bluetooth/hci.h>
 
-static bool hrf_ntf_enabled;
+#include <zephyr/bluetooth/services/ots.h>
+
+#define DEVICE_NAME      CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN  (sizeof(DEVICE_NAME) - 1)
+
+#define OBJ_POOL_SIZE CONFIG_BT_OTS_MAX_OBJ_CNT
+#define OBJ_MAX_SIZE  100
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
-		      BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
-		      BT_UUID_16_ENCODE(BT_UUID_BAS_VAL),
-		      BT_UUID_16_ENCODE(BT_UUID_DIS_VAL)),
-#if defined(CONFIG_BT_EXT_ADV)
-	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
-#endif /* CONFIG_BT_EXT_ADV */
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 };
 
-#if !defined(CONFIG_BT_EXT_ADV)
 static const struct bt_data sd[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(BT_UUID_OTS_VAL)),
 };
-#endif /* !CONFIG_BT_EXT_ADV */
 
-/* Use atomic variable, 2 bits for connection and disconnection state */
-static ATOMIC_DEFINE(state, 2U);
+static struct {
+	uint8_t data[OBJ_MAX_SIZE];
+	char name[CONFIG_BT_OTS_OBJ_MAX_NAME_LEN + 1];
+} objects[OBJ_POOL_SIZE];
+static uint32_t obj_cnt;
 
-#define STATE_CONNECTED    1U
-#define STATE_DISCONNECTED 2U
+struct object_creation_data {
+	struct bt_ots_obj_size size;
+	char *name;
+	uint32_t props;
+};
+
+#define OTS_OBJ_ID_TO_OBJ_IDX(id) (((id) - BT_OTS_OBJ_ID_MIN) % ARRAY_SIZE(objects))
+
+static struct object_creation_data *object_being_created;
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
 	if (err) {
-		printk("Connection failed, err 0x%02x %s\n", err, bt_hci_err_to_str(err));
-	} else {
-		printk("Connected\n");
-
-		(void)atomic_set_bit(state, STATE_CONNECTED);
+		printk("Connection failed, err %u %s\n", err, bt_hci_err_to_str(err));
+		return;
 	}
+
+	printk("Connected\n");
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	printk("Disconnected, reason 0x%02x %s\n", reason, bt_hci_err_to_str(reason));
-
-	(void)atomic_set_bit(state, STATE_DISCONNECTED);
+	printk("Disconnected, reason %u %s\n", reason, bt_hci_err_to_str(reason));
 }
 
 BT_CONN_CB_DEFINE(conn_callbacks) = {
@@ -66,129 +68,258 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.disconnected = disconnected,
 };
 
-static void hrs_ntf_changed(bool enabled)
+static int ots_obj_created(struct bt_ots *ots, struct bt_conn *conn, uint64_t id,
+			   const struct bt_ots_obj_add_param *add_param,
+			   struct bt_ots_obj_created_desc *created_desc)
 {
-	hrf_ntf_enabled = enabled;
+	char id_str[BT_OTS_OBJ_ID_STR_LEN];
+	uint64_t index;
 
-	printk("HRS notification status changed: %s\n",
-	       enabled ? "enabled" : "disabled");
-}
+	bt_ots_obj_id_to_str(id, id_str, sizeof(id_str));
 
-static struct bt_hrs_cb hrs_cb = {
-	.ntf_changed = hrs_ntf_changed,
-};
-
-static void auth_cancel(struct bt_conn *conn)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Pairing cancelled: %s\n", addr);
-}
-
-static struct bt_conn_auth_cb auth_cb_display = {
-	.cancel = auth_cancel,
-};
-
-static void bas_notify(void)
-{
-	uint8_t battery_level = bt_bas_get_battery_level();
-
-	battery_level--;
-
-	if (!battery_level) {
-		battery_level = 100U;
+	if (obj_cnt >= ARRAY_SIZE(objects)) {
+		printk("No item from Object pool is available for Object "
+		       "with %s ID\n", id_str);
+		return -ENOMEM;
 	}
 
-	bt_bas_set_battery_level(battery_level);
-}
-
-static void hrs_notify(void)
-{
-	static uint8_t heartrate = 90U;
-
-	/* Heartrate measurements simulation */
-	heartrate++;
-	if (heartrate == 160U) {
-		heartrate = 90U;
+	if (add_param->size > OBJ_MAX_SIZE) {
+		printk("Object pool item is too small for Object with %s ID\n",
+		       id_str);
+		return -ENOMEM;
 	}
 
-	if (hrf_ntf_enabled) {
-		bt_hrs_notify(heartrate);
+	if (object_being_created) {
+		created_desc->name = object_being_created->name;
+		created_desc->size = object_being_created->size;
+		created_desc->props = object_being_created->props;
+	} else {
+		index = id - BT_OTS_OBJ_ID_MIN;
+		objects[index].name[0] = '\0';
+
+		created_desc->name = objects[index].name;
+		created_desc->size.alloc = OBJ_MAX_SIZE;
+		BT_OTS_OBJ_SET_PROP_READ(created_desc->props);
+		BT_OTS_OBJ_SET_PROP_WRITE(created_desc->props);
+		BT_OTS_OBJ_SET_PROP_PATCH(created_desc->props);
+		BT_OTS_OBJ_SET_PROP_DELETE(created_desc->props);
 	}
-}
 
-#if defined(CONFIG_GPIO)
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-#if DT_NODE_HAS_STATUS_OKAY(LED0_NODE)
-#include <zephyr/drivers/gpio.h>
-#define HAS_LED     1
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-#define BLINK_ONOFF K_MSEC(500)
-
-static struct k_work_delayable blink_work;
-static bool                  led_is_on;
-
-static void blink_timeout(struct k_work *work)
-{
-	led_is_on = !led_is_on;
-	gpio_pin_set(led.port, led.pin, (int)led_is_on);
-
-	k_work_schedule(&blink_work, BLINK_ONOFF);
-}
-
-static int blink_setup(void)
-{
-	int err;
-
-	printk("Checking LED device...");
-	if (!gpio_is_ready_dt(&led)) {
-		printk("failed.\n");
-		return -EIO;
-	}
-	printk("done.\n");
-
-	printk("Configuring GPIO pin...");
-	err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (err) {
-		printk("failed.\n");
-		return -EIO;
-	}
-	printk("done.\n");
-
-	k_work_init_delayable(&blink_work, blink_timeout);
+	printk("Object with %s ID has been created\n", id_str);
+	obj_cnt++;
 
 	return 0;
 }
 
-static void blink_start(void)
+static int ots_obj_deleted(struct bt_ots *ots, struct bt_conn *conn,
+			    uint64_t id)
 {
-	printk("Start blinking LED...\n");
-	led_is_on = false;
-	gpio_pin_set(led.port, led.pin, (int)led_is_on);
-	k_work_schedule(&blink_work, BLINK_ONOFF);
+	char id_str[BT_OTS_OBJ_ID_STR_LEN];
+
+	bt_ots_obj_id_to_str(id, id_str, sizeof(id_str));
+
+	printk("Object with %s ID has been deleted\n", id_str);
+
+	obj_cnt--;
+
+	return 0;
 }
 
-static void blink_stop(void)
+static void ots_obj_selected(struct bt_ots *ots, struct bt_conn *conn,
+			     uint64_t id)
 {
-	struct k_work_sync work_sync;
+	char id_str[BT_OTS_OBJ_ID_STR_LEN];
 
-	printk("Stop blinking LED.\n");
-	k_work_cancel_delayable_sync(&blink_work, &work_sync);
+	bt_ots_obj_id_to_str(id, id_str, sizeof(id_str));
 
-	/* Keep LED on */
-	led_is_on = true;
-	gpio_pin_set(led.port, led.pin, (int)led_is_on);
+	printk("Object with %s ID has been selected\n", id_str);
 }
-#endif /* LED0_NODE */
-#endif /* CONFIG_GPIO */
+
+static ssize_t ots_obj_read(struct bt_ots *ots, struct bt_conn *conn,
+			    uint64_t id, void **data, size_t len,
+			    off_t offset)
+{
+	char id_str[BT_OTS_OBJ_ID_STR_LEN];
+	uint32_t obj_index = OTS_OBJ_ID_TO_OBJ_IDX(id);
+
+	bt_ots_obj_id_to_str(id, id_str, sizeof(id_str));
+
+	if (!data) {
+		printk("Object with %s ID has been successfully read\n",
+		       id_str);
+
+		return 0;
+	}
+
+	*data = &objects[obj_index].data[offset];
+
+	/* Send even-indexed objects in 20 byte packets
+	 * to demonstrate fragmented transmission.
+	 */
+	if ((obj_index % 2) == 0) {
+		len = (len < 20) ? len : 20;
+	}
+
+	printk("Object with %s ID is being read\n"
+		"Offset = %lu, Length = %zu\n",
+		id_str, (long)offset, len);
+
+	return len;
+}
+
+static ssize_t ots_obj_write(struct bt_ots *ots, struct bt_conn *conn,
+			     uint64_t id, const void *data, size_t len,
+			     off_t offset, size_t rem)
+{
+	char id_str[BT_OTS_OBJ_ID_STR_LEN];
+	uint32_t obj_index = OTS_OBJ_ID_TO_OBJ_IDX(id);
+
+	bt_ots_obj_id_to_str(id, id_str, sizeof(id_str));
+
+	printk("Object with %s ID is being written\n"
+		"Offset = %lu, Length = %zu, Remaining= %zu\n",
+		id_str, (long)offset, len, rem);
+
+	(void)memcpy(&objects[obj_index].data[offset], data, len);
+
+	return len;
+}
+
+static void ots_obj_name_written(struct bt_ots *ots, struct bt_conn *conn,
+				 uint64_t id, const char *cur_name, const char *new_name)
+{
+	char id_str[BT_OTS_OBJ_ID_STR_LEN];
+
+	bt_ots_obj_id_to_str(id, id_str, sizeof(id_str));
+
+	printk("Name for object with %s ID is being changed from '%s' to '%s'\n",
+		id_str, cur_name, new_name);
+}
+
+static int ots_obj_cal_checksum(struct bt_ots *ots, struct bt_conn *conn, uint64_t id,
+				off_t offset, size_t len, void **data)
+{
+	uint32_t obj_index = OTS_OBJ_ID_TO_OBJ_IDX(id);
+
+	if (obj_index >= OBJ_POOL_SIZE) {
+		return -ENOENT;
+	}
+
+	*data = &objects[obj_index].data[offset];
+	return 0;
+}
+
+static struct bt_ots_cb ots_callbacks = {
+	.obj_created = ots_obj_created,
+	.obj_deleted = ots_obj_deleted,
+	.obj_selected = ots_obj_selected,
+	.obj_read = ots_obj_read,
+	.obj_write = ots_obj_write,
+	.obj_name_written = ots_obj_name_written,
+	.obj_cal_checksum = ots_obj_cal_checksum,
+};
+
+static int ots_init(void)
+{
+	int err;
+	struct bt_ots *ots;
+	struct object_creation_data obj_data;
+	struct bt_ots_init_param ots_init;
+	struct bt_ots_obj_add_param param;
+	const char * const first_object_name = "first_object.txt";
+	const char * const second_object_name = "second_object.gif";
+	uint32_t cur_size;
+	uint32_t alloc_size;
+
+	ots = bt_ots_free_instance_get();
+	if (!ots) {
+		printk("Failed to retrieve OTS instance\n");
+		return -ENOMEM;
+	}
+
+	/* Configure OTS initialization. */
+	(void)memset(&ots_init, 0, sizeof(ots_init));
+	BT_OTS_OACP_SET_FEAT_READ(ots_init.features.oacp);
+	BT_OTS_OACP_SET_FEAT_WRITE(ots_init.features.oacp);
+	BT_OTS_OACP_SET_FEAT_CREATE(ots_init.features.oacp);
+	BT_OTS_OACP_SET_FEAT_DELETE(ots_init.features.oacp);
+	BT_OTS_OACP_SET_FEAT_PATCH(ots_init.features.oacp);
+	BT_OTS_OLCP_SET_FEAT_GO_TO(ots_init.features.olcp);
+	ots_init.cb = &ots_callbacks;
+
+	/* Initialize OTS instance. */
+	err = bt_ots_init(ots, &ots_init);
+	if (err) {
+		printk("Failed to init OTS (err:%d)\n", err);
+		return err;
+	}
+
+	/* Prepare first object demo data and add it to the instance. */
+	cur_size = sizeof(objects[0].data) / 2;
+	alloc_size = sizeof(objects[0].data);
+	for (uint32_t i = 0; i < cur_size; i++) {
+		objects[0].data[i] = i + 1;
+	}
+
+	(void)memset(&obj_data, 0, sizeof(obj_data));
+	__ASSERT(strlen(first_object_name) <= CONFIG_BT_OTS_OBJ_MAX_NAME_LEN,
+		 "Object name length is larger than the allowed maximum of %u",
+		 CONFIG_BT_OTS_OBJ_MAX_NAME_LEN);
+	(void)strcpy(objects[0].name, first_object_name);
+	obj_data.name = objects[0].name;
+	obj_data.size.cur = cur_size;
+	obj_data.size.alloc = alloc_size;
+	BT_OTS_OBJ_SET_PROP_READ(obj_data.props);
+	BT_OTS_OBJ_SET_PROP_WRITE(obj_data.props);
+	BT_OTS_OBJ_SET_PROP_PATCH(obj_data.props);
+	object_being_created = &obj_data;
+
+	param.size = alloc_size;
+	param.type.uuid.type = BT_UUID_TYPE_16;
+	param.type.uuid_16.val = BT_UUID_OTS_TYPE_UNSPECIFIED_VAL;
+	err = bt_ots_obj_add(ots, &param);
+	object_being_created = NULL;
+	if (err < 0) {
+		printk("Failed to add an object to OTS (err: %d)\n", err);
+		return err;
+	}
+
+	/* Prepare second object demo data and add it to the instance. */
+	cur_size = sizeof(objects[0].data);
+	alloc_size = sizeof(objects[0].data);
+	for (uint32_t i = 0; i < cur_size; i++) {
+		objects[1].data[i] = i * 2;
+	}
+
+	(void)memset(&obj_data, 0, sizeof(obj_data));
+	__ASSERT(strlen(second_object_name) <= CONFIG_BT_OTS_OBJ_MAX_NAME_LEN,
+		 "Object name length is larger than the allowed maximum of %u",
+		 CONFIG_BT_OTS_OBJ_MAX_NAME_LEN);
+	(void)strcpy(objects[1].name, second_object_name);
+	obj_data.name = objects[1].name;
+	obj_data.size.cur = cur_size;
+	obj_data.size.alloc = alloc_size;
+	BT_OTS_OBJ_SET_PROP_READ(obj_data.props);
+	object_being_created = &obj_data;
+
+	param.size = alloc_size;
+	param.type.uuid.type = BT_UUID_TYPE_16;
+	param.type.uuid_16.val = BT_UUID_OTS_TYPE_UNSPECIFIED_VAL;
+	err = bt_ots_obj_add(ots, &param);
+	object_being_created = NULL;
+	if (err < 0) {
+		printk("Failed to add an object to OTS (err: %d)\n", err);
+		return err;
+	}
+
+	return 0;
+}
 
 int main(void)
 {
 	int err;
+
+	printk("Starting Bluetooth Peripheral OTS example\n");
 
 	err = bt_enable(NULL);
 	if (err) {
@@ -198,110 +329,18 @@ int main(void)
 
 	printk("Bluetooth initialized\n");
 
-	bt_conn_auth_cb_register(&auth_cb_display);
+	err = ots_init();
+	if (err) {
+		printk("Failed to init OTS (err:%d)\n", err);
+		return 0;
+	}
 
-	bt_hrs_cb_register(&hrs_cb);
-
-#if !defined(CONFIG_BT_EXT_ADV)
-	printk("Starting Legacy Advertising (connectable and scannable)\n");
 	err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 	if (err) {
 		printk("Advertising failed to start (err %d)\n", err);
 		return 0;
 	}
 
-#else /* CONFIG_BT_EXT_ADV */
-	struct bt_le_adv_param adv_param = {
-		.id = BT_ID_DEFAULT,
-		.sid = 0U,
-		.secondary_max_skip = 0U,
-		.options = (BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_CONN | BT_LE_ADV_OPT_CODED),
-		.interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
-		.interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
-		.peer = NULL,
-	};
-	struct bt_le_ext_adv *adv;
-
-	printk("Creating a Coded PHY connectable non-scannable advertising set\n");
-	err = bt_le_ext_adv_create(&adv_param, NULL, &adv);
-	if (err) {
-		printk("Failed to create Coded PHY extended advertising set (err %d)\n", err);
-
-		printk("Creating a non-Coded PHY connectable non-scannable advertising set\n");
-		adv_param.options &= ~BT_LE_ADV_OPT_CODED;
-		err = bt_le_ext_adv_create(&adv_param, NULL, &adv);
-		if (err) {
-			printk("Failed to create extended advertising set (err %d)\n", err);
-			return 0;
-		}
-	}
-
-	printk("Setting extended advertising data\n");
-	err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (err) {
-		printk("Failed to set extended advertising data (err %d)\n", err);
-		return 0;
-	}
-
-	printk("Starting Extended Advertising (connectable non-scannable)\n");
-	err = bt_le_ext_adv_start(adv, BT_LE_EXT_ADV_START_DEFAULT);
-	if (err) {
-		printk("Failed to start extended advertising set (err %d)\n", err);
-		return 0;
-	}
-#endif /* CONFIG_BT_EXT_ADV */
-
 	printk("Advertising successfully started\n");
-
-#if defined(HAS_LED)
-	err = blink_setup();
-	if (err) {
-		return 0;
-	}
-
-	blink_start();
-#endif /* HAS_LED */
-
-	/* Implement notification. */
-	while (1) {
-		k_sleep(K_SECONDS(1));
-
-		/* Heartrate measurements simulation */
-		hrs_notify();
-
-		/* Battery level simulation */
-		bas_notify();
-
-		if (atomic_test_and_clear_bit(state, STATE_CONNECTED)) {
-			/* Connected callback executed */
-
-#if defined(HAS_LED)
-			blink_stop();
-#endif /* HAS_LED */
-		} else if (atomic_test_and_clear_bit(state, STATE_DISCONNECTED)) {
-#if !defined(CONFIG_BT_EXT_ADV)
-			printk("Starting Legacy Advertising (connectable and scannable)\n");
-			err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd,
-					      ARRAY_SIZE(sd));
-			if (err) {
-				printk("Advertising failed to start (err %d)\n", err);
-				return 0;
-			}
-
-#else /* CONFIG_BT_EXT_ADV */
-			printk("Starting Extended Advertising (connectable and non-scannable)\n");
-			err = bt_le_ext_adv_start(adv, BT_LE_EXT_ADV_START_DEFAULT);
-			if (err) {
-				printk("Failed to start extended advertising set (err %d)\n", err);
-				return 0;
-			}
-#endif /* CONFIG_BT_EXT_ADV */
-
-#if defined(HAS_LED)
-			blink_start();
-#endif /* HAS_LED */
-		}
-	}
-
 	return 0;
 }
